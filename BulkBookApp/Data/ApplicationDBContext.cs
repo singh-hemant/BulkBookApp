c6973953f59a0a162eeff642d98e589a7f6b6a41
AzureVMHemant <AzureVMHemant@az204-vm1>
2024-03-11 04:01:22 +0000

    Add project files.

---
+++ BulkBookApp/Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using BulkBookApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkBookApp.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        { 
+        }
+        DbSet<Category> categories {  get; set; }
+    }
+}
